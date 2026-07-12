3a0b37f51064a8b7211b6f8cfb6cfe3bf056e646
Raja Muhammad Ehsan Ali Khan <ehsankiani222@gmail.com>
2026-07-12 11:12:20 -0700

    Added Live Search

--- src/components/PatientTable.jsx
+++ src/components/PatientTable.jsx
@@ -4,7 +4,11 @@ import "../styles/PatientTable.css";
 import PatientRow from "./PatientRow";
 
 function PatientTable() {
-  const { patients } = useContext(PatientContext);
+  const { patients, searchTerm } = useContext(PatientContext);
+
+  const filteredPatients = patients.filter((patient) =>
+    patient.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div className="patient-table">
@@ -19,7 +23,7 @@ function PatientTable() {
         </thead>
 
         <tbody>
-          {patients.map((patient) => (
+          {filteredPatients.map((patient) => (
             <PatientRow
               key={patient.id}
               name={patient.name}
@@ -28,6 +32,17 @@ function PatientTable() {
               status={patient.status}
             />
           ))}
+
+          {filteredPatients.length === 0 && (
+            <tr>
+              <td
+                colSpan="4"
+                style={{ textAlign: "center", padding: "20px" }}
+              >
+                No patient found.
+              </td>
+            </tr>
+          )}
         </tbody>
       </table>
     </div>
